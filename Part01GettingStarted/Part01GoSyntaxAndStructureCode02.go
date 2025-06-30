5a355a41d7bb11ba8b3ff599a982b28d61b4e43b
LAPTOP-5P9IL7SD\lialu <dcalibri@gmail.com>
2025-06-30 23:04:30 +0700

    Checkpoint - Part01

---
+++ Part01GettingStarted/Part01GoSyntaxAndStructureCode02.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "DevCalibri"
+	age := 22 //Shorthand
+	fmt.Println("My Name", name, "Age:", age)
+}
+
+// Deklarasi Variable AGE
+// Deklarasi ------- NAMA
+// Run Via Powershell
+// Hasilnya:
+// My Name DevCalibri Age: 22
---
+++ Part01GettingStarted/Part01GoSyntaxAndStructureCode03.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(name string) string {
+	return "Hello, " + name
+}
+
+func main() {
+	message := greet("DevCalibri")
+	fmt.Println(message)
+}
+
+// Deklarasi Variable AGE
+// Deklarasi ------- NAMA
+// Cara kerja main skeskusi -> call fungsi greet, print the message yang diambil di line 12
+// Run Via Powershell
+// Hasilnya:
+//
